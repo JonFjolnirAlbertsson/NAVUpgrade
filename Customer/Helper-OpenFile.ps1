a43939d8123501afd5af5da9d6e96dcf3b1331a8
JonFjolnirAlbertsson <jal@si-data.no>
2016-04-09 17:27:58 +0200

    Script to run and open files in Notepad or Kdiff

---
+++ Customer/Helper-OpenFile.ps1
@@ -0,0 +1,6 @@
+﻿$ObjectName ='TAB39'
+Open-File-SID -ObjectName $ObjectName -OpenInNotepadPlus -OpenMerged -OpenModified -OpenTarget -WorkingFolder 'C:\NAVUpgrade\Customer\SI-Data\NAV2013R2\CU29\Upgrade_NAV71CU29SIData'
+
+$WindowsUser = 'SI-DATA\JAL'
+New-NAVServerUser -ServerInstance $UpgradeName -WindowsAccount $WindowsUser
+New-NAVServerUserPermissionSet -ServerInstance $UpgradeName -WindowsAccount $WindowsUser -PermissionSetId 'SUPER' -ErrorAction Continue
