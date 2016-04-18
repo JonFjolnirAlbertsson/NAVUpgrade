c55e32a4612bb7bd59e3c78f9ffadac57f3a92fd
JonFjolnirAlbertsson <jal@si-data.no>
2016-04-18 21:32:29 +0200

    Helper file

--- Customer/Helper-OpenFile.ps1
+++ Customer/Helper-OpenFile.ps1
@@ -1,6 +1,5 @@
-﻿$ObjectName ='TAB39'
-Open-File-SID -ObjectName $ObjectName -OpenInNotepadPlus -OpenMerged -OpenModified -OpenTarget -WorkingFolder 'C:\NAVUpgrade\Customer\SI-Data\NAV2013R2\CU29\Upgrade_NAV71CU29SIData'
+﻿$Location = "C:\GitHub\NAVUpgrade\Customer\Elas\Script"
+. (join-path $Location 'Set-UpgradeSettings.ps1')
+$ObjectName ='TAB27'
+Open-File-SID -ObjectName $ObjectName -OpenInNotepadPlus -OpenOriginal -OpenModified -OpenMerged -OpenTarget -OpenToBeJoined -WorkingFolder $WorkingFolder
 
-$WindowsUser = 'SI-DATA\JAL'
-New-NAVServerUser -ServerInstance $UpgradeName -WindowsAccount $WindowsUser
-New-NAVServerUserPermissionSet -ServerInstance $UpgradeName -WindowsAccount $WindowsUser -PermissionSetId 'SUPER' -ErrorAction Continue
--- SQL/Find missing dimension values.sql
+++ SQL/Find missing dimension values.sql
@@ -1,15 +1,15 @@
-select * from
 
-  [NAV2016_BuschUpgrade].[dbo].[Busch Vakuumteknikk AS$Ledger Entry Dimension] A
-
+select * from [NAV90CU5Elas].[dbo].[Elas AS$Ledger Entry Dimension] A
  where A.[Dimension Value Code] not in 
+ (select B.Code from
+[NAV90CU5Elas].[dbo].[AS Elas Eiendom$Dimension Value] B)
 
- (
+ select * from [NAV90CU5Elas].[dbo].[AS Elas Eiendom$Ledger Entry Dimension] A
+ where A.[Dimension Value Code] not in (
+ select B.Code from
+[NAV90CU5Elas].[dbo].[AS Elas Eiendom$Dimension Value] B)
 
- select B.Code
-
- from
-
- [NAV2016_BuschUpgrade].[dbo].[Busch Vakuumteknikk AS$Dimension Value] B
-
- )
+ select * from [NAV90CU5Elas].[dbo].[Sokalbygget AS$Ledger Entry Dimension] A
+ where A.[Dimension Value Code] not in (
+ select B.Code from
+[NAV90CU5Elas].[dbo].[Sokalbygget AS$Dimension Value] B)
