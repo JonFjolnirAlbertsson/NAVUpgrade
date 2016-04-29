bbed274c095b17b83f9eba4d5579196c113b9d76
JonFjolnirAlbertsson <jal@si-data.no>
2016-04-29 11:15:00 +0200

    Changes helper files

--- Customer/Helper-OpenFile.ps1
+++ Customer/Helper-OpenFile.ps1
@@ -1,5 +1,5 @@
-﻿$Location = "C:\GitHub\NAVUpgrade\Customer\Elas\Script"
-. (join-path $Location 'Set-UpgradeSettings.ps1')
-$ObjectName ='TAB27'
-Open-File-SID -ObjectName $ObjectName -OpenInNotepadPlus -OpenOriginal -OpenModified -OpenMerged -OpenTarget -OpenToBeJoined -WorkingFolder $WorkingFolder
+﻿#$Location = "C:\GitHub\NAVUpgrade\Customer\SNLA\Script"
+#. (join-path $Location 'Set-UpgradeSettings.ps1')
+$ObjectName ='TAB15000004'
+Open-File-SID -ObjectName $ObjectName -OpenInNotepadPlus -OpenOriginal -OpenModified -OpenTarget -WorkingFolder $WorkingFolderNAV2009
 
--- Customer/Helper.ps1
+++ Customer/Helper.ps1
@@ -27,3 +27,34 @@ Join-NAVApplicationObjectFile -Source "C:\NAVUpgrade\Customer\SI-Data\NAV 2016\I
 New-NAVServerInstance -ManagementServicesPort 7045 -ServerInstance $ServerInstance -ClientServicesCredentialType Windows -ClientServicesPort 7046 -DatabaseName $UpgradeName -DatabaseServer $DBServer -ODataServicesPort 7048 -SOAPServicesPort 7047
 Enable-NAVServerInstancePortSharing -ServerInstance $ServerInstance
 
+$TestDataBaseName = 'NAV80CU17ElasPerfTest'
+$StartedDateTime = Get-Date
+$BackupFileName = $UpgradeDataBaseName + "_Step7.bak"
+$BackupFilePath = join-path $BackupPath $BackupFileName 
+Backup-SqlDatabase -ServerInstance $DBServer -Database $TestDataBaseName -BackupAction Database -BackupFile $BackupFilePath -CompressionOption Default
+$StoppedDateTime = Get-Date
+Write-Host 'Start at: ' + $StartedDateTime + ' . Finished at: ' + $StoppedDateTime + ' . Total time' + ($StoppedDateTime-$StartedDateTime) -ForegroundColor Yellow
+
+$StartedDateTime = Get-Date
+Restore-SQLBackupFile-SID -BackupFile $BackupFilePath -DatabaseName $TestDataBaseName
+$StoppedDateTime = Get-Date
+Write-Host 'Start at: ' + $StartedDateTime + ' . Finished at: ' + $StoppedDateTime + ' . Total time' + ($StoppedDateTime-$StartedDateTime) -ForegroundColor Yellow
+ 
+New-NAVEnvironment -Databasename $TestDataBaseName -DatabaseServer $DBServer -EnablePortSharing -LicenseFile $NAVLicense -ServerInstance $TestDataBaseName
+
+Sync-NAVTenant -ServerInstance $TestDataBaseName -Mode CheckOnly
+Sync-NAVTenant -ServerInstance $TestDataBaseName -Force
+
+# Start Data upgrade NAV 2016
+$StartedDateTime = Get-Date
+Start-NAVDataUpgrade -ServerInstance $TestDataBaseName -FunctionExecutionMode Serial
+Resume-NAVDataUpgrade -ServerInstance $TestDataBaseName
+Stop-NAVDataUpgrade -ServerInstance $TestDataBaseName
+# Follow up the data upgrade process
+Get-NAVDataUpgrade -ServerInstance $TestDataBaseName -Progress
+Get-NAVDataUpgrade -ServerInstance $TestDataBaseName -Detailed | ogv
+#Get-NAVDataUpgrade -ServerInstance $NavServiceInstance -Detailed | Out-File 
+Get-NAVDataUpgrade -ServerInstance $TestDataBaseName -ErrorOnly | ogv
+
+$StoppedDateTime = Get-Date
+Write-Host 'Start at: ' + $StartedDateTime + ' . Finished at: ' + $StoppedDateTime + ' . Total time' + ($StoppedDateTime-$StartedDateTime) -ForegroundColor Yellow
