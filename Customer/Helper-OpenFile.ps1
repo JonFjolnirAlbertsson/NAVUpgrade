89678d25e2085f8cdb85336a34b05fd32aa0a478
JonFjolnirAlbertsson <jal@si-data.no>
2016-04-29 15:19:15 +0200

    Helper files

--- Customer/Helper-OpenFile.ps1
+++ Customer/Helper-OpenFile.ps1
@@ -1,5 +1,5 @@
-﻿$Location = "C:\GitHub\NAVUpgrade\Customer\Elas\Script"
-. (join-path $Location 'Set-UpgradeSettings.ps1')
-$ObjectName ='TAB27'
+﻿#$Location = "C:\GitHub\NAVUpgrade\Customer\Elas\Script"
+#. (join-path $Location 'Set-UpgradeSettings.ps1')
+$ObjectName ='PAG254'
 Open-File-SID -ObjectName $ObjectName -OpenInNotepadPlus -OpenOriginal -OpenModified -OpenMerged -OpenTarget -OpenToBeJoined -WorkingFolder $WorkingFolder
 
---
+++ Customer/Merge.ps1
@@ -0,0 +1,40 @@
+﻿#Base Path
+$RootFolderPath = "C:\NavUpgrade"
+#NAV
+$VersionListPrefixes = 'NAVW1', 'NAVNO', 'I'
+$NAVLicense = "$RootFolderPath\License\NAV2016.flf"
+$CompareObject = '*.TXT'
+#Folders and files
+$WorkingFolder = join-path $RootFolderPath 'Customer\Sandven'
+$TargetFile = join-path $WorkingFolder 'Sandven_NAV2009_COD.txt'
+$TargetFile = join-path $WorkingFolder 'Sandven_NAV2009_TAB.txt'
+$TargetFile = join-path $WorkingFolder 'Sandven_NAV2009_PAG.txt'
+$TargetFile = join-path $WorkingFolder 'Sandven_NAV2009.txt'
+$TargetFolder = join-path $WorkingFolder 'Target'
+$ModifiedFile = join-path $WorkingFolder 'Compello_NAV2013_R2_CU7_NO.txt'
+$ModifiedFolder = join-path $WorkingFolder 'Modified'
+$OriginalFile = join-path $WorkingFolder 'NAV2013_R2_CU7_NO.txt'
+$OriginalFolder = join-path $WorkingFolder 'Original'
+$ResultFolder = join-path $WorkingFolder 'Result'
+$ResultFile = join-path $WorkingFolder 'MergeResult.txt'
+#Databases and backup folder and files
+$BackupfileCaompanyDB = ''
+
+Split-NAVApplicationObjectFile -Source $TargetFile -Destination $TargetFolder
+Split-NAVApplicationObjectFile -Source $ModifiedFile -Destination $ModifiedFolder
+Split-NAVApplicationObjectFile -Source $OriginalFile -Destination $OriginalFolder
+
+#New-NAVEnvironment -ServerInstance $UpgradeObjectsName  -BackupFile $BackupfileCaompanyDB -ErrorAction Stop -EnablePortSharing -LicenseFile $NAVLicense
+Merge-NAVCode -WorkingFolderPath $WorkingFolder -CompareObject $CompareObject -Join
+Merge-NAVCode -WorkingFolderPath $WorkingFolder -CompareObject $CompareObject -Join
+
+$MergeResult = Merge-NAVUpgradeObjects `
+    -OriginalObjects $OriginalFile `    -ModifiedObjects $ModifiedFile `
+    -TargetObjects $TargetFile `
+    -WorkingFolder $WorkingFolder `
+    -VersionListPrefixes $VersionListPrefixes `
+    -Force
+
+Split-NAVApplicationObjectFile -Source $ResultFile -Destination $ResultFolder
+
+Merge-NAVCode -WorkingFolderPath $WorkingFolder -CompareObject $CompareObject -Join
