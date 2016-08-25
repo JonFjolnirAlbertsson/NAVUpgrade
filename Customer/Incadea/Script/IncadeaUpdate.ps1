92a050f2ee83dfd75ae675c1baf921a37237053c
JonFjolnirAlbertsson <jal@si-data.no>
2016-08-25 11:09:04 +0200

    Changed name to Incadea

---
+++ Customer/Incadea/Script/IncadeaUpdate.ps1
@@ -0,0 +1,18 @@
+﻿$CompanyName = 'Incadea'
+$Location = "C:\GitHub\NAVUpgrade\Customer\$CompanyName\Script"
+. (join-path $Location 'Set-IncadeaUpdateSettings.ps1')
+
+
+clear-host
+
+$StartedDateTime = Get-Date
+
+#Reset Workingfolder
+if (test-path $WorkingFolder){
+    if (Confirm-YesOrNo -title 'Remove WorkingFolder?' -message "Do you want to remove the WorkingFolder $WorkingFolder ?"){
+        Remove-Item -Path $WorkingFolder -Force -Recurse
+    } else {
+        write-error '$WorkingFolder already exists.  Overwrite not allowed.'
+        break
+    }
+}
---
+++ Customer/Incadea/Script/Set-IncadeaUpdateSettings.ps1
@@ -0,0 +1,45 @@
+﻿#NAV Specific data
+$NAVShortVersion = '71'
+$NAVVersion = '2013R2'
+$NAVCU = 'CU29'
+$VersionListPrefixes = 'NAVW1', 'NAVNO', 'I'
+#Company data
+$UpgradeName = 'NAV' + $NAVShortVersion  + $NAVCU + $CompanyName
+$UpdateNo = '14'
+$UpdateFileName = 'Changesv14.txt'
+#General
+$INCUpdateFolder = "Incadea\FOB\Update $UpdateNo"
+$CompanyFolder = "$CompanyName\NAV$NAVVersion\$NAVCU"
+$WorkingFolder = "C:\NAVUpgrade\Customer\$CompanyFolder\$INCUpdateFolder\$UpgradeName"
+$NAVLicense = 'C:\NAVUpgrade\License\NAV2016.flf'
+$VersionListPrefixes = 'NAVW1', 'NAVNO', 'I'
+
+$ImportLog = join-path $WorkingFolder 'Log'
+$ConversionLog = join-path $WorkingFolder 'Log'
+
+#Servers
+$DBServer = 'localhost'
+$NAVServer = 'localhost'
+
+#Original Version
+$OriginalVersion = $UpgradeName
+$OriginalObjects = join-path $WorkingFolder "$($OriginalVersion).txt"
+$OriginalFolder = join-path $WorkingFolder 'Original'
+
+#Modified Version
+$ModifiedFolder = join-path $WorkingFolder 'Modified'
+$ModifiedServerInstance = $UpgradeName
+$ModifiedObjects = join-path $ModifiedFolder $UpdateFileName
+
+#Target Version
+$TargetVersion = $UpgradeName
+$TargetServerInstance = 'NAV71SIData'
+$TargetObjects = join-path $WorkingFolder "$($TargetVersion).txt"
+$TargetFolder = join-path $WorkingFolder 'Target'
+
+#Result Version
+$ResultObjectFile = Join-Path $WorkingFolder 'Result.fob'
+
+#Join
+$JoinFolder = join-path $WorkingFolder 'Merged\ToBeJoined'
+$DestinationFile = join-path $WorkingFolder "$ModifiedServerInstance.txt"
