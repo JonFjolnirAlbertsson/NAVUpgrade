725c0c537abe8b008b79c0d06f03a84bd92123f5
Jón Fjölnir Albertsson <jal@si-data.no>
2016-12-14 13:53:07 +0100

    Incadea Contract Maintenance

---
+++ Customer/Incadea/Script/IncadeaMC.ps1
@@ -0,0 +1,15 @@
+﻿$WorkingFolder = 'C:\NAVUpgrade\Customer\Incadea\NAV2013R2\CU29\Incadea\FOB\MC\Original'
+$OriginalObjects = 'Tables.txt'
+$OriginalObjects = join-path $WorkingFolder $OriginalObjects
+Merge-NAVCode -WorkingFolderPath $WorkingFolder -ModifiedFileName $OriginalObjects -Split
+$OriginalObjects = 'CodeUnits.txt'
+$OriginalObjects = join-path $WorkingFolder $OriginalObjects
+Merge-NAVCode -WorkingFolderPath $WorkingFolder -ModifiedFileName $OriginalObjects -Split
+$OriginalObjects = 'Pages.txt'
+$OriginalObjects = join-path $WorkingFolder $OriginalObjects
+Merge-NAVCode -WorkingFolderPath $WorkingFolder -ModifiedFileName $OriginalObjects -Split
+#Merge-NAVCode -WorkingFolderPath $WorkingFolder -OriginalFileName $OriginalObjects -ModifiedFileName $ModifiedObjects -TargetFileName $TargetObjects -Merge
+Merge-NAVCode -WorkingFolderPath $WorkingFolder -Join 
+# Merge Customer database objects and NAV 2016 objects.
+#$MergeResult = Merge-NAV2013R2UpgradeObjects -OriginalObjects $OriginalObjects -ModifiedObjects $ModifiedObjects -TargetObjects $TargetObjects -WorkingFolder $WorkingFolder -VersionListPrefixes $VersionListPrefixes -Force
+
---
+++ Customer/Incadea/Script/IncadeaUpdate15-16.ps1
@@ -0,0 +1,12 @@
+﻿$WorkingFolder = 'C:\NAVUpgrade\Customer\Incadea\NAV2013R2\CU29\Incadea\FOB\Update15-16\Merge'
+$ModifiedObjects = 'TAB17 and 98 and 50003.txt'
+$ModifiedObjects = join-path $WorkingFolder $ModifiedObjects
+Merge-NAVCode -WorkingFolderPath $WorkingFolder -ModifiedFileName $ModifiedObjects -Split
+$ModifiedObjects = 'COD 12 and 40 and 50000.txt'
+$ModifiedObjects = join-path $WorkingFolder $ModifiedObjects
+Merge-NAVCode -WorkingFolderPath $WorkingFolder -ModifiedFileName $ModifiedObjects -Split
+#Merge-NAVCode -WorkingFolderPath $WorkingFolder -OriginalFileName $OriginalObjects -ModifiedFileName $ModifiedObjects -TargetFileName $TargetObjects -Merge
+Merge-NAVCode -WorkingFolderPath $WorkingFolder -Join 
+# Merge Customer database objects and NAV 2016 objects.
+#$MergeResult = Merge-NAV2013R2UpgradeObjects -OriginalObjects $OriginalObjects -ModifiedObjects $ModifiedObjects -TargetObjects $TargetObjects -WorkingFolder $WorkingFolder -VersionListPrefixes $VersionListPrefixes -Force
+
