7ab9b6c9c7826858c73b113d6b67f14d7ae9de8d
Jón Fjölnir Albertsson <fjolnir.albertsson@incadea.com>
2017-05-04 11:39:22 +0200

    Øveraasen

---
+++ Customer/Øveraasen/Script/Helper-OpenFile.ps1
@@ -0,0 +1,15 @@
+﻿$CompanyName = 'Øveraasen'
+#$CompanyName = 'NOBI'
+#$CompanyName = 'Incadea\FastFit'
+$Location = "C:\GitHub\NAVUpgrade\Customer\$CompanyName\Script"
+. (join-path $Location ('Set-UpgradeSettings.ps1'))
+$WorkingFolder = 'C:\incadea\Customer\Øveraasen\NAV2017\CU03'
+#. (join-path $Location ('NAV2017-Settings.ps1'))
+#Open Files with conflict. Using the Merge Folder to merge the conflict
+#$ObjectName ='COD430'
+$ObjectName ='FOR21'
+Open-File-SID -WorkingFolder $WorkingFolder -ObjectName $ObjectName -OpenInNotepadPlus -OpenOriginal -OpenModified #-OpenTarget -OpenMerged -OpenToBeJoined
+
+#Open Files with no conflict from MergedResult or the Result folder 
+#$ObjectName ='COD5906'
+#Open-File-SID -ObjectName $ObjectName -WorkingFolder $WorkingFolder -OpenInNotepadPlus -UseWaldoFolders
