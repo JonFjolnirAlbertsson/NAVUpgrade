8c82a4837fff87315c07896d72b74a9ac4dc4dfa
Jón Fjölnir Albertsson <fjolnir.albertsson@incadea.com>
2017-05-04 11:39:32 +0200

    FastFit helper

--- Customer/Helper-OpenFile.ps1
+++ Customer/Helper-OpenFile.ps1
@@ -3,12 +3,13 @@
 #$CompanyName = 'Incadea\FastFit'
 $Location = "C:\GitHub\NAVUpgrade\Customer\$CompanyName\Script"
 . (join-path $Location ('Set-UpgradeSettings.ps1'))
+$WorkingFolder = 'C:\incadea\Customer\Øveraasen\NAV2017\CU03'
 #. (join-path $Location ('NAV2017-Settings.ps1'))
 #Open Files with conflict. Using the Merge Folder to merge the conflict
 #$ObjectName ='COD430'
-$ObjectName ='TAB5407'
-Open-File-SID -WorkingFolder $WorkingFolder -ObjectName $ObjectName -OpenInNotepadPlus -OpenOriginal -OpenModified -OpenTarget -OpenMerged -OpenToBeJoined
+$ObjectName ='FOR21'
+Open-File-SID -WorkingFolder $WorkingFolder -ObjectName $ObjectName -OpenInNotepadPlus -OpenOriginal -OpenModified #-OpenTarget -OpenMerged -OpenToBeJoined
 
 #Open Files with no conflict from MergedResult or the Result folder 
-3#$ObjectName ='COD5906'
+#$ObjectName ='COD5906'
 #Open-File-SID -ObjectName $ObjectName -WorkingFolder $WorkingFolder -OpenInNotepadPlus -UseWaldoFolders
---
+++ Customer/Incadea/FastFit/Script/Helper-OpenFile.ps1
@@ -0,0 +1,3 @@
+﻿$WorkingFolder = 'C:\incadea\fastfit\NAV2016\CU17\Upgrade_FastFit'
+$ObjectName ='TAB36'
+Open-File-SID -ObjectName $ObjectName -WorkingFolder $WorkingFolder -OpenInNotepadPlus -UseWaldoFolders
