8eb9b0a29f8404c810ddd3d89ea03e795d03cf48
Jón Fjölnir Albertsson <fjolnir.albertsson@incadea.com>
2017-06-22 09:11:41 +0200

    FastFit

--- Customer/Incadea/FastFit/Script/Helper-OpenFile.ps1
+++ Customer/Incadea/FastFit/Script/Helper-OpenFile.ps1
@@ -1,4 +1,4 @@
 ﻿$WorkingFolder = 'C:\incadea\Fastfit\083000\NAV2016\CU17\Upgrade_Fastfit'
-$ObjectName ='TAB370'
-Open-File-SID -ObjectName $ObjectName -WorkingFolder $WorkingFolder -OpenInNotepadPlus -UseWaldoFolders
+$ObjectName ='FOR99000831'
+#Open-File-SID -ObjectName $ObjectName -WorkingFolder $WorkingFolder -OpenInNotepadPlus -UseWaldoFolders
 Open-File-SID -ObjectName $ObjectName -WorkingFolder $WorkingFolder -OpenInNotepadPlus -OpenTarget -OpenOriginal
