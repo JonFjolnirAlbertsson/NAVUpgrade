576983e6d692c025bbee7479e1fc5df6c981c109
Jón Fjölnir Albertsson <fjolnir.albertsson@incadea.com>
2017-09-06 13:44:33 +0200

    Fastfit SQL statistic

--- Customer/Helper-OpenFile.ps1
+++ Customer/Helper-OpenFile.ps1
@@ -3,13 +3,16 @@
 #$CompanyName = 'Incadea\FastFit'
 $Location = "C:\Git\NAVUpgrade\Customer\$CompanyName\Script"
 . (join-path $Location ('Set-UpgradeSettings.ps1'))
-$WorkingFolder = 'C:\incadea\Customer\Øveraasen\NAV2017\CU03'
+#$WorkingFolder = 'C:\incadea\Customer\Øveraasen\NAV2017\CU03'
+$WorkingFolder = join-path 'C:\Git\NAV AddOns' 'EHF\PDF'
+
 #. (join-path $Location ('NAV2017-Settings.ps1'))
 #Open Files with conflict. Using the Merge Folder to merge the conflict
 #$ObjectName ='COD430'
-$ObjectName ='TAB58006'
+$ObjectName ='COD10619'
 Open-File-SID -WorkingFolder $WorkingFolder -ObjectName $ObjectName -OpenInNotepadPlus -OpenOriginal -OpenModified #-OpenTarget -OpenMerged -OpenToBeJoined
 
 #Open Files with no conflict from MergedResult or the Result folder 
 #$ObjectName ='COD5906'
 #Open-File-SID -ObjectName $ObjectName -WorkingFolder $WorkingFolder -OpenInNotepadPlus -UseWaldoFolders
+Split-NAVApplicationObjectFile -Source 'C:\Git\NAV AddOns\ICA\All New and Changed objects.txt' -Destination 'C:\Git\NAV AddOns\ICA\Modified'
