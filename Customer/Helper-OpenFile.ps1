e545ab049631821c035ea96845a1d5d33274c8cc
Jón Fjölnir Albertsson <fjolnir.albertsson@incadea.com>
2018-01-31 15:28:43 +0100

    fastfit moving files to backup folder

--- Customer/Helper-OpenFile.ps1
+++ Customer/Helper-OpenFile.ps1
@@ -1,23 +1,14 @@
 ﻿Set-Location -Path (Split-Path $psise.CurrentFile.FullPath -Qualifier)
-#$Location = 
-$CompanyName = 'PP'
-#$CompanyName = 'Incadea\FastFit'
+#$CompanyName = 'PP'
+$CompanyName = 'Incadea\FastFit'
 $Location = join-path (Split-Path $psise.CurrentFile.FullPath) "$CompanyName\Script"
 . (join-path $Location ('Set-UpgradeSettings.ps1'))
-#$WorkingFolder = "C:\incadea\Customer\$CompanyName \NAV2018\CU01"
-#$WorkingFolder = join-path 'C:\Git\NAV AddOns' 'EHF\PDF'
-
-#. (join-path $Location ('NAV2017-Settings.ps1'))
 #Open Files with conflict. Using the Merge Folder to merge the conflict
 #$ObjectName ='COD430'
 #<#
-$ObjectName ='TAB81'
-Open-File-SID -WorkingFolder $WorkingFolder -ObjectName $ObjectName -OpenInNotepadPlus -OpenMerged #-OpenTarget -OpenMerged -OpenToBeJoined
-Open-File-SID -ObjectName $ObjectName -WorkingFolder $WorkingFolder -OpenInNotepadPlus -UseWaldoFolders
+$ObjectName ='COD80'
+Open-File-INC -WorkingFolder $WorkingFolder -ObjectName $ObjectName -OpenInNotepadPlus -OpenMerged #-OpenTarget -OpenMerged -OpenToBeJoined
+Open-File-INC -WorkingFolder $WorkingFolder -ObjectName $ObjectName -OpenModified -OpenOriginal -OpenTarget -UseWaldoFolders -OpenInNotepadPlus
+
 #>
 
-#Remove-OriginalFilesNotInTarget -WorkingFolderPath $WorkingFolder -WriteResultToFile
-#Open Files with no conflict from MergedResult or the Result folder 
-#$ObjectName ='COD5906'
-#Open-File-SID -ObjectName $ObjectName -WorkingFolder $WorkingFolder -OpenInNotepadPlus -UseWaldoFolders
-#Split-NAVApplicationObjectFile -Source 'C:\Git\NAV AddOns\ICA\All New and Changed objects.txt' -Destination 'C:\Git\NAV AddOns\ICA\Modified'
---
+++ Customer/Incadea/FastFit/Script/Backup/Set-UpgradeSettings_old.ps1
@@ -0,0 +1,86 @@
+﻿#Base Path
+$RootFolderPath = Join-Path 'F:\' 'incadea'
+#Servers
+$DBServer = 'NO01DEVSQL01'
+$NAVServer = 'NO01DEV03'
+#NAV Specific data
+$NAVShortVersion = 'NAV90'
+$NavServiceInstance = "DynamicsNAV90"
+$NAVVersion = 'NAV2016'
+$NAVCU = 'CU17'
+$FastFitNAVCU = 'CU17'
+$VersionListPrefixes = 'NAVW1', 'NAVNO', 'I'
+$NAVLicense = "$RootFolderPath\License\incadea.fastfit_8.X (NAV2016)_development_INS-NOR_4805448_20170321.flf"
+#$UserName = 'incadea\albertssonf'
+$UserName = 'si-dev\devjal'
+$InstanceUserName = 'nav_user@si-dev.local'
+$InstancePassword = '1378Nesbru'
+$DBUser = 'NAV_Service'
+#Company data
+$CompanyName = 'Fastfit'
+#$VersionFolder = '083000'
+$VersionFolder = '084010'
+$CompanyFolder = "$CompanyName\$VersionFolder"
+$RootFolder = join-path $RootFolderPath $CompanyFolder
+$WorkingFolder = join-path $RootFolder "\$NAVVersion\$NAVCU\Upgrade_$CompanyName"
+#Original DB objects
+$ObjectLibrary = "$RootFolderPath\DB Original"
+$ObjectLibraryW1 = "$RootFolderPath\DB Original\W1"
+$ObjectLibraryNO = "$RootFolderPath\DB Original\NO"
+#Database backup files
+$BackupPath = join-path $RootFolder 'Database Backups'
+$DemoDBName = 'Demo Database NAV (9-0) CU17 W1'
+$TargetDemoDBName = 'Demo Database NAV (9-0) CU17 NO'
+$BackupfileNAVTargetDemoDB = join-path $ObjectLibraryNO 'Demo Database NAV (9-0) CU17.bak'
+$BackupfileNAVDemoDB = join-path $ObjectLibraryW1 'Demo Database NAV (9-0) CU17.bak'
+$AppDBName = 'fastfit_083000_NO_APP'
+$AppDBNameW1 = 'fastfit_083000_W1_APP'
+$BackupfileAppDB = join-path $BackupPath ('fastfit_' + $VersionFolder + 'W1_APP.bak')
+$DEALER1DBName = 'fastfit_' + $VersionFolder + '_NO_DEALER1'
+$DEALER1DBNameW1 = 'fastfit_' + $VersionFolder + '_W1_DEALER1'
+$BackupfileDEALER1DB = join-path $BackupPath ('fastfit_' + $VersionFolder + 'W1_DEALER1.bak')
+$Dealer1Tenant= 'dealer1'
+$Dealer1TenantW1= 'dealer1w1'
+$FastFitInstance = 'fastfit_' + $VersionFolder + '_NO'
+$FastFitInstanceDev = 'fastfit_' + $VersionFolder + '_NO_Dev'
+$FastFitInstanceW1 = 'fastfit_' + $VersionFolder + '_W1'
+
+#Merging parameters
+$SourcePath = join-path $WorkingFolder 'MergeResult' 
+$ConflictTarget = join-path $SourcePath  'ConflictTarget' 
+$MergedPath = join-path $WorkingFolder 'Merged' 
+$JoinPath = Join-Path $WorkingFolder 'Merged\ToBeJoined\'
+$JoinFile = join-path $WorkingFolder 'all-merged-objects.txt'
+#Putting the paths toghether
+$LogPath = "$RootFolder\Logs\"
+$CompileLog = $LogPath + "compile"
+$ImportLog = $LogPath + "import"
+$ConversionLog = $LogPath + "Conversion"
+$TmpLocation = "$NAVRootFolder\Temp\"
+$ISODir = "$NAVRootFolder\ISO"
+$NAVRootFolder = "$RootFolderPath\NAV\NAV$NAVVersion"
+$InstallLogFolder = "$NAVRootFolder\Log"
+$InstallLog = "$InstallLogFolder\install.log"
+
+#Original Version
+$OriginalVersion = $NAVVersion + '_' + $FastFitNAVCU + '_W1'
+$OriginalObjects = join-path $ObjectLibrary "$($OriginalVersion).txt"
+$OriginalFolder = join-path $WorkingFolder 'Original'
+
+#Modified Version
+$FastFitServerInstance = $UpgradeName
+$FastFitObjects = join-path $RootFolder 'Installation\NAV Objects\incadea.fastfit 08.30.00 W1 All Objects Excluded DEU.txt'
+$FastFitFolder = join-path $WorkingFolder 'Modified'
+
+#Target Version
+$TargetVersion = $NAVVersion + '_' + $NAVCU + '_NO' + '_Without_NOR'
+$TargetServerInstance = $TargetVersion + '_' + $CompanyName
+$TargetObjects = join-path $ObjectLibrary "$($TargetVersion).txt"
+$TargetFolder = join-path $WorkingFolder 'Target'
+
+#Result Version
+$ResultObjectFile = Join-Path $WorkingFolder 'Result.fob'
+
+#Join
+$JoinFolder = join-path $WorkingFolder 'Merged\ToBeJoined'
+$DestinationFile = join-path $WorkingFolder "$ModifiedServerInstance.txt"
