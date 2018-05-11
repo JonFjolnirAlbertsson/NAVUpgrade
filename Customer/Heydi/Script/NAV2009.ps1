eadbf8b8d314ab20288aa23b5d9f6ece1ab4f458
Jón Fjölnir Albertsson <fjolnir.albertsson@incadea.com>
2018-05-11 15:43:23 +0200

    SQL update

--- Customer/Heydi/Script/NAV2009.ps1
+++ Customer/Heydi/Script/NAV2009.ps1
@@ -1,5 +1,10 @@
 ﻿$OrgFile = 'C:\incadea\Customer\Heydi\NAV2009\NAV2009_R2_32012_NO_Heydi_AllObjects.txt'
 $NewFile = 'C:\incadea\Customer\Heydi\NAV2009\NAV2009_R2_32012_NO_Heydi_AllObjects_New.txt'
+$FormsFile = 'C:\incadea\Customer\Heydi\NAV2009\NAV2009_R2_32012_NO_Heydi_AllObjects - Forms.txt'
+$NewFormsFile = 'C:\incadea\Customer\Heydi\NAV2009\NAV2009_R2_32012_NO_Heydi_AllObjects - Forms New.txt'
 $DestFolder = 'C:\incadea\Customer\Heydi\NAV2009\Modified'
+$DestFormsFolder = 'C:\incadea\Customer\Heydi\NAV2009\Forms'
 Split-NAVApplicationObjectFile -Source $OrgFile -Destination $DestFolder -PreserveFormatting -Force
 Join-NAVApplicationObjectFile -Source "$DestFolder\*.TXT" -Destination $NewFile -Force
+Split-NAVApplicationObjectFile -Source $FormsFile -Destination $DestFormsFolder -PreserveFormatting -Force
+Join-NAVApplicationObjectFile -Source "$DestFormsFolder\*.TXT" -Destination $NewFormsFile -Force
